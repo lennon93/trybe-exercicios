65c4b640ee8e0a62a07213e683de029dd0097fa7
Lennon <lennon_lxs@hotmail.com>
2022-09-25 15:05:36 -0300

    Termina Exercicio 3

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -18,3 +18,14 @@ const perimeter = 2 * base + 2 * height;
 
 console.log(perimeter);
 
+//===================================================//
+
+let nota = 79;
+
+if(nota >= 80) {
+    console.log("Parabéns, você foi aprovado com a nota de: " + nota + '!')
+} else if (nota < 80 && nota >= 60) {
+    console.log('Você está na nossa lista de espera!')
+} else {
+    console.log('Você foi reprovado!')
+}
