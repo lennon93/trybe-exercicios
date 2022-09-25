b3d32cccf3e3be76772443811a39157bbe64e06a
Lennon <lennon_lxs@hotmail.com>
2022-09-25 14:24:07 -0300

    Exercicios 1 e 2

---
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,20 @@
+const myName = 'Lennon';
+const birthCity = 'São Vicente';
+let birthYear = 1993;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+//===================================================//
+
+const base = 5;
+const height = 8;
+const area = base * height;
+ 
+console.log(area);
+
+const perimeter = 2 * base + 2 * height;
+
+console.log(perimeter);
+
