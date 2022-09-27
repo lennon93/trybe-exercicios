3c1ecb6bbbcc538145d7745b94a85f4133a34422
Lennon <lennon_lxs@hotmail.com>
2022-09-26 22:45:58 -0300

    Termina os exercicios basicos

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/scripts.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,2 +1,33 @@
- 
- 
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//=========================================//
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+menu.push('Contato');
+
+console.log(menu);
+
+//=========================================//
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for ( let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
+
+//=========================================//
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+    console.log(name);
+  }
+
+//=========================================//
