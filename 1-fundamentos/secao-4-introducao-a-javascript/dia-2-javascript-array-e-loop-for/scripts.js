a06a03679a0d6c3504ec408cc0b2a4c4fbbb5f76
Lennon <lennon_lxs@hotmail.com>
2022-09-27 00:34:59 -0300

    Termina o dia 2

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/scripts.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/scripts.js
@@ -31,3 +31,67 @@ for (let name of names) {
   }
 
 //=========================================//
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index +=1) {
+  console.log(numbers[index])
+}
+
+let sum = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+  sum = sum + numbers[index] 
+}  
+console.log(sum);
+
+let media = sum / numbers.length;
+
+console.log(media);
+
+if (media > 20){
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
+
+let biggestNumber = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+  if (numbers[index] > biggestNumber) {
+    biggestNumber = numbers[index];
+  }
+  
+}
+
+console.log(biggestNumber);
+
+let oddNumber = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+  if (numbers[index] % 2 !== 0) {
+    oddNumber += 1;
+  }
+  
+}
+
+console.log(oddNumber);
+
+//=========================================//
+
+let newNumbers = [];
+
+for (let index = 1; index <= 25; index +=1) {
+    newNumbers.push(index);
+  
+}
+
+console.log(newNumbers);
+
+for (let index = 0; index < newNumbers.length; index +=1) {
+  let division = newNumbers[index] / 2;
+  console.log(division);
+  
+}
+
