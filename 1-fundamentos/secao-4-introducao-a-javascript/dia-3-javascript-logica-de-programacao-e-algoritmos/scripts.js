23557aabf1bed85108876a1b2f3ef535e965f8d2
Lennon <lennon_lxs@hotmail.com>
2022-09-27 18:10:43 -0300

    Termina exercicios regulares do dia 3

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -24,7 +24,7 @@ let biggestWord = array[0];
 let smallestWord = array[0];
 
 
-for (index = 0; index < array.length; index +=1) {
+for (let index = 0; index < array.length; index +=1) {
     if (array[index].length > biggestWord.length) {
         biggestWord = array[index];
     }  if (array[index].length < smallestWord.length) {
@@ -35,3 +35,18 @@ for (index = 0; index < array.length; index +=1) {
 console.log(biggestWord);
 console.log(smallestWord);
 
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 7; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
