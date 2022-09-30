96f91aad7e520c5772f07d7facfd099ce8994105
Lennon <lennon_lxs@hotmail.com>
2022-09-29 23:55:42 -0300

    Exercicio 3

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -14,3 +14,25 @@ let car = {
   for(let key in car) {
     console.log(key, car[key]);
   }
+
+  function sum(num1, num2) {
+    return num1 + num2;
+  }
+
+  function minum(num1, num2) {
+    return num1 - num2;
+  }
+
+  function division(num1, num2) {
+    return num1 / num2;
+  }
+
+  function multiplication(num1, num2) {
+    return num1 * num2;
+  }
+
+  function modulo(num1, num2) {
+    return num1 % num2;
+  }
+
+ console.log(sum(5,2));
