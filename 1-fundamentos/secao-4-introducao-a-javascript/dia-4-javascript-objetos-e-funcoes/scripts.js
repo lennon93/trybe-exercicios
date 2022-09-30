1278c3327536f480a72269a79a803f823aeb88ef
Lennon <lennon_lxs@hotmail.com>
2022-09-30 14:46:04 -0300

    Exercicios do palindromo e do index do maior número

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -12,7 +12,7 @@ let car = {
   };
 
   for(let key in car) {
-    console.log(key, car[key]);
+    //console.log(key, car[key]);
   }
 
   function sum(num1, num2) {
@@ -35,4 +35,30 @@ let car = {
     return num1 % num2;
   }
 
- console.log(sum(5,2));
+ //console.log(sum(5,2));
+
+ function verificaPalindrome(word) {
+  let wordNew = word.split('');
+  let reverse = wordNew.reverse();
+  let reverseWord = reverse.join('');
+  if (reverseWord === word) {
+    return true;
+  } 
+  return false;
+ }
+
+
+ //console.log(verificaPalindrome('arara'));
+
+
+ function biggestInteger(arrayInteger) {
+   let biggestNumber = Math.max(...arrayInteger);
+    let indexInteger = arrayInteger.indexOf(biggestNumber);
+    return indexInteger;
+ }
+ 
+ 
+ 
+ //console.log(biggestInteger([1,2,3]));
+
+ 
