18fe19a300e2f85858d67eed84cfb839b2561046
Lennon <lennon_lxs@hotmail.com>
2022-09-29 23:46:05 -0300

    Exercicio 2

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -5,3 +5,12 @@ age : 34,
 medals : { golden: 2, silver: 3 },
 }
 
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for(let key in car) {
+    console.log(key, car[key]);
+  }
