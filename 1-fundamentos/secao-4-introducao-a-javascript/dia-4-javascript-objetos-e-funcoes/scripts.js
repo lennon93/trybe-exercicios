7e145784f34039b21f90225d4bc0706b4c97a3e9
Lennon <lennon_lxs@hotmail.com>
2022-10-01 00:57:32 -0300

    Termina exercicios regulares

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -114,3 +114,24 @@ function sumNumero(num){
 
 console.log(sumNumero(5));
 
+
+
+function verificaFimPalavra(palavra, fimPalavra) {
+  let inversoPalavra = palavra.split('').reverse().join('');
+  let inversoFimPalavra = fimPalavra.split('').reverse().join('');
+  let result;
+  for (let index = 0; index < inversoFimPalavra.length; index += 1) {
+    if (inversoPalavra[index] !== inversoFimPalavra[index]) {
+      result = false;
+      break;
+      break; // O "break" serve para encerrar o loop.
+    } else {
+      result = true;
+    }
+  }
+  
+  return result;
+}
+
+console.log(verificaFimPalavra('trybe', 'be')); //true
+console.log(verificaFimPalavra('joaofernando', 'fernan')); //false
