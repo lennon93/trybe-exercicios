f4d122b8c4b91986685a26aa005a0910eb98f630
Lennon <lennon_lxs@hotmail.com>
2022-10-03 21:38:51 -0300

    Termina exercicios 2

---
+++ 1-fundamentos/secao-5-javaScript-DOM-eventos-e-web-storage/dia-1-javasript-dom-e-seletores/script.js
@@ -0,0 +1,22 @@
+let header = document.getElementById('header-container');
+header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+
+let footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'rgb(0, 176, 105)';
+
+let emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
+emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)';
+
+let emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
+  emergencyTasksHeaders[index].style.backgroundColor = 'rgb(165, 0, 243)';
+}
+
+let noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+noEmergencyTasks.style.backgroundColor = 'rgb(249, 219, 94)';
+
+let noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
+for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+  noEmergencyTasksHeaders[index].style.backgroundColor = 'rgb(35, 37, 37)';
+}
