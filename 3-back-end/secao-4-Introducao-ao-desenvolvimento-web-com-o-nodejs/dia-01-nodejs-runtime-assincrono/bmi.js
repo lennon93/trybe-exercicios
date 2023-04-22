4f116a1fa22d990b16ecab9550320535057f21cb
Lennon <lennon_lxs@hotmail.com>
2023-04-22 14:48:48 -0300

    Exercicio 4 e 5

--- 3-back-end/secao-4-Introducao-ao-desenvolvimento-web-com-o-nodejs/dia-01-nodejs-runtime-assincrono/bmi.js
+++ 3-back-end/secao-4-Introducao-ao-desenvolvimento-web-com-o-nodejs/dia-01-nodejs-runtime-assincrono/bmi.js
@@ -1,5 +1,45 @@
 const readline = require('readline-sync');
 
+const BMI_MAX_AND_MIN = {
+    'Underweight': {
+      minBMI: 0,
+      maxBMI: 18.4,
+    },
+    'Normal Weight': {
+      minBMI: 18.5,
+      maxBMI: 24.9,
+    },
+    'Overweight': {
+      minBMI: 25,
+      maxBMI: 29.9,
+    },
+    'Obese Class I': {
+      minBMI: 30.0,
+      maxBMI: 34.9,
+    },
+    'Obese Class II': {
+      minBMI: 35,
+      maxBMI: 39.9,
+    },
+    'Obese Class III': {
+      minBMI: 40,
+      maxBMI: 100,
+    },
+  };
+
+  function handleBMIResult(bmi) {
+    const statuses = Object.keys(BMI_MAX_AND_MIN); 
+  
+    const resultFind = statuses.find((status) => {
+      const { maxBMI, minBMI } = BMI_MAX_AND_MIN[status]; 
+  
+    
+      return bmi >= minBMI && bmi <= maxBMI;
+    });
+  
+    return resultFind;
+  }
+
 function handleBMI(weight, height) {
   console.log(`Weight: ${weight}, Height: ${height}`);
 
@@ -12,12 +52,13 @@ function handleBMI(weight, height) {
 }
 
 function main() {
-    const weight = readline.questionInt('What\'s your weight? (kg) ');
+    const weight = readline.questionFloat('What\'s your weight? (kg) ');
     const height = readline.questionInt('What\'s your height? (cm) ');
  
     const bmi = handleBMI(weight, height);
+    const bmiResult = handleBMIResult(bmi);
 
-  console.log(`BMI: ${bmi.toFixed(2)}`);
+  console.log(bmiResult);
 }
 
 main();
