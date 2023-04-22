0ef2e4739397546cc97d54fc96a51d5a1c8910b9
Lennon <lennon_lxs@hotmail.com>
2023-04-22 18:02:51 -0300

    exercicio 8

--- 3-back-end/secao-4-Introducao-ao-desenvolvimento-web-com-o-nodejs/dia-2-node-js-API-REST-com-express/app.js
+++ 3-back-end/secao-4-Introducao-ao-desenvolvimento-web-com-o-nodejs/dia-2-node-js-API-REST-com-express/app.js
@@ -55,5 +55,19 @@ app.get('/movies/:id', async (req, res) => {
     }
   });
 
+  app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const index = movies.findIndex((element) => element.id === Number(id));
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
+res.status(200).json(movies[index]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
 
 module.exports = app;
