2d1e51510d88a80c9eccf468ce8852c6d67f3328
Lennon <lennon_lxs@hotmail.com>
2023-04-23 18:10:32 -0300

    Cria a rota de signup e o gerador de token e importa no app

--- 3-back-end/secao-4-Introducao-ao-desenvolvimento-web-com-o-nodejs/dia-4-node-js-express-e-middleware/app.js
+++ 3-back-end/secao-4-Introducao-ao-desenvolvimento-web-com-o-nodejs/dia-4-node-js-express-e-middleware/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const validateDifficulty = require('./middlewares/validateDifficulty');
 const validateRating = require('./middlewares/validateRating');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateDescription = require('./middlewares/validateDescription');
@@ -9,7 +10,20 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/signup', (_req, res) => {
+    const { email, password, firstName, phone } = req.body;
+  
+    if ([email, password, firstName, phone].includes(undefined)) {
+      return res.status(401).json({ message: 'Campos ausentes!' });
+    }
+  
+    const token = generateToken();
+  
+    return res.status(200).json({ token });
+  });
+
 app.post('/activities',
+  validateDifficulty,
   validateRating,
   validateCreatedAt,
   validateDescription,
@@ -18,7 +32,5 @@ app.post('/activities',
   (_req, res) => {
   res.status(201).json({ message: 'Atividade registrada com sucesso!' });
 });
-
-module.exports = app;
 
 module.exports = app;
