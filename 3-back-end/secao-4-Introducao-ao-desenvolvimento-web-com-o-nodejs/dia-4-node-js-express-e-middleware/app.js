4bfd0cd74502c9a024037f74c0fa4479a436a97f
Lennon <lennon_lxs@hotmail.com>
2023-04-23 18:00:19 -0300

    Cria o middleware de validar preço e importa no app

--- 3-back-end/secao-4-Introducao-ao-desenvolvimento-web-com-o-nodejs/dia-4-node-js-express-e-middleware/app.js
+++ 3-back-end/secao-4-Introducao-ao-desenvolvimento-web-com-o-nodejs/dia-4-node-js-express-e-middleware/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const validatePrice = require('./middlewares/validatePrice');
+const validateName = require('./middlewares/validateName');
+
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', (_req, res) => {
+app.post('/activities',
+  validatePrice,
+  validateName,
+  (_req, res) => {
   res.status(201).json({ message: 'Atividade registrada com sucesso!' });
 });
 
