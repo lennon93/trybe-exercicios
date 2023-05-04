a6985c7d74dfd98b1b62a68839d0f75c9b392b1b
Lennon <lennon_lxs@hotmail.com>
2023-05-04 11:43:40 -0300

    exercicios dia 1

---
+++ 2-front-end/dia-2-introducao-react/my-app/src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+        <>
+        <h1>Lennon Xavier</h1>
+        <p>Desenvolvedor Web</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+            <li>React</li>
+            <li>HTML</li>
+            <li>Redux</li>
+        </ul>
+        </>
+    );
+  }
+}
+
+export default About;
