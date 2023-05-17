b2c0b8d45917ccb8fed6d292deac9e368bae9464
Lennon <lennon_lxs@hotmail.com>
2023-05-17 15:12:34 -0300

    Exercicio dia 2 props

---
+++ 2-front-end/dia-2-introducao-react/exercicios-de-fixacao-props/exercise-image-component/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from './Image';
+import staringCat from './staringCat.jpg';
+
+function App() {
+  return (
+    <main>
+      <Image source={ staringCat } alternativeText="Cute cat staring" />
+    </main>
+  );
+}
+
+export default App;
